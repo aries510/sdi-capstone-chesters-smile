ed9459544a8823ccbd4ba4c4fa34fe6e75960934
davy-sdi <davyy2811@gmail.com>
2026-09-03 15:53:52 -0400

    AdminHome.jsx basic

--- where-my-troops-at/src/AdminHome.jsx
+++ where-my-troops-at/src/AdminHome.jsx
@@ -1,12 +1,26 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 import './App.css'
 
-function App() {
-    const [evaluators, setEvaluators] = useState(0)
+function AdminHome() {
 
     return (
-        <></>
+        <div>
+            <h1>Admin Dashboard</h1>
+            <div className='admin-top-row'></div>
+            <div className='eval-trainee-list'>Evaluators and Trainees</div>
+            <div className='admin-actions'>
+                <div className='upload-button'>UPLOAD DOCUMENT</div>
+                <div className='import-button'>+ BULK IMPORT PDF/CSV</div>
+            </div>
+
+            <div className='admin-panels'>
+                <EvaluatorsPanel />
+                <CertQualRenewalPanel />
+            </div>
+            <CertificanCatalog />
+        </div>
+
     )
 }
 
-export default App
+export default AdminHome
