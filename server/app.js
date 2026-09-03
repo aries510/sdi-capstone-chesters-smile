f2139af8c7dbd5daaf775047b516658933516c48
aries510 <darenrespicio@gmail.com>
2026-09-03 14:07:07 -0600

    pulled database setup so i can test routes with postman

--- server/app.js
+++ server/app.js
@@ -2,16 +2,57 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const cors = require('cors');
+const knex = require('knex')(require('./knexfile.js')['development']);
 
 app.use(express.json());
 app.use(cors());
 
+// API homepage route
 app.get('/', (request, response) => {
     response.status(200).send('Chester Smiles API Homepage....')
-})
+});
 
+// /users route that returns users table
+app.get('/users', (request, response) => {
+    knex('users')
+        .select('*')
+        .then(users => response.json(users))
+});
 
+// /domain route that returns domain table
+app.get('/domain', (request, response) => {
+    knex('domain')
+        .select('*')
+        .then(domain => response.json(domain))
+});
 
+// /personnel route that returns personnel table
+app.get('/personnel', (request, response) => {
+    knex('personnel')
+        .select('*')
+        .then(personnel => response.json(personnel))
+});
 
+// /weaponsystems route that returns weapon_systems table
+app.get('/weaponsystems', (response, request) => {
+    knex('weapon_systems')
+        .select('*')
+        .then(systems => response.json(systems))
+});
 
+// /crewroles route that returns crew_roles tablea
+app.get('crew_roles', (request, response) =>{
+    knex('crew_roles')
+        .select('*')
+        .then(roles => response.json(roles))
+});
+
+// /certs route returns certifications table
+app.get('/certs', (request, response) => {
+    knex('certifications')
+        .select('*')
+        .then(certs => response.json(certs))
+});
+
+//
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
