9aebabab5ca12cdc34683bde9c5d8a15079a3c43
aries510 <darenrespicio@gmail.com>
2026-09-08 09:29:27 -0600

    merge conflict resolved

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cors = require('cors');
+const knex = require('knex')(require('./knexfile.js')['development']);
+
+const users = require('./routes/users.js')
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/users', users)
 
 // API homepage route
 app.get('/', (request, response) => {
@@ -18,17 +23,56 @@ app.get('/brew', (req, res) => {
   });
 });
 
-// /users route that returns users table
-app.use('/users', require('./routes/users'));
+/* /users route that returns users table
+transferred to users.js via express.router
 
-// /domains route that returns domains table
-app.use('/domains', require('./routes/domains'));
+app.get('/users', (request, response) => {
+  knex('users')
+    .select('*')
+    .then((users) => response.json(users));
+});
+*/
+
+// /domain route that returns domain table
+app.get('/domain', (request, response) => {
+    knex('domains')
+        .select('*')
+        .then(domain => response.json(domain))
+});
+
+// Route for domain per the id
+app.get('/domain/:id', (request, response) => {
+    const { id } = request.params
+
+    knex('domains')
+        .where('id', id)
+        .first()
+        .then(domain => response.json(domain))
+});
 
 // /personnel route that returns personnel table
-app.use('/personnel', require('./routes/personnel'));
+app.get('/personnel/:id', (request, response) => {
+    const { id } = request.params;
+
+    knex('personnel')
+        .where('id', id)
+        .first()
+        .then(personnel => response.json(personnel))
+});
 
 // /weaponsystems route that returns weapon_systems table
-app.use('/weaponsystems', require('./routes/weaponSystems'));
+app.get('/weaponsystems', (request, response) => {
+    knex('weapon_systems')
+        .join('domains', 'weapon_systems.domain_id', '=', 'domains.id')
+        .select(
+            'weapon_systems.id', 
+            'weapon_systems.name', 
+            'weapon_systems.acronym', 
+            'weapon_systems.description', 
+            'domains.name as domain'
+        )
+        .then(systems => response.json(systems))
+});
 
 // /crewroles route that returns crew_roles tablea
 app.use('/crewroles', require('./routes/crewRoles'));
@@ -37,7 +81,22 @@ app.use('/crewroles', require('./routes/crewRoles'));
 app.use('/certs', require('./routes/certs'));
 
 // /quals route returns crew_qualifications table
-app.use('/quals', require('./routes/crewQualifications'));
+app.get('/quals', (request, response) => {
+  knex('crew_qualifications')
+    .select('*')
+    .then((quals) => response.json(quals));
+    knex('crew_qualifications')
+        .innerJoin('personnel', 'crew_qualifications.personnel_id', '=', 'personnel.id')
+        .innerJoin('crew_roles', 'crew_qualifications.crew_role_id', '=', 'crew_roles.id')
+        .innerJoin('weapon_systems', 'weapon_systems.id', '=', 'crew_qualifications.system_id')
+        .select(
+            'crew_qualifications.id',
+            'personnel.last_name as personnel',
+            'crew_roles.name as crew_roles',
+            'weapon_systems.name as weapon_systems'
+        )
+        .then(quals => response.json(quals))
+});
 
 // /perscerts route returns personnel_certifications table
 app.use('/perscerts', require('./routes/personnelCertifications'));
